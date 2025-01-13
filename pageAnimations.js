61b485febfc4159d24f2ff618c2344b7f914dcdc
daberan <mail@davidberan.com>
2025-01-14 00:52:07 +0100

    hi

--- pageAnimations.js
+++ pageAnimations.js
@@ -1,3 +1,17 @@
 function onLoadAnimations() {
+  // cat onload move up
   document.querySelector(".canvasCat").classList.add("onLoadMove");
+  // text onload move up
+  document.querySelector(".textWrapper").classList.add("onLoadMove");
+  // button onload move up
+  setTimeout(() => {
+    document.querySelector(".button").classList.add("onLoadMove");
+  }, 650);
+}
+
+function openMouthMove() {
+  openMouth();
+  document.querySelector(".canvasCat").classList.add("openMouthMove");
+  document.querySelector("nav").classList.add("moveUp");
+  document.querySelector(".tongue").classList.add("moveUpAndDown");
 }
